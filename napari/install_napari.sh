27cd5a736e433a5d03cf4b03120918420065cbd1
vanessadao31 <vanessadao31@yahoo.co.uk>
2024-01-25 16:26:12 +0000

    remote_ikernel

--- napari/install_napari.sh
+++ napari/install_napari.sh
@@ -78,7 +78,9 @@ fi
 echo "Created environment"
 
 # set up jupyter notebook kernels
+ml purge
 conda activate napari-nemo-jan24
+pip install remote_ikernel
 python -m ipykernel install --user --name=napari-nemo-jan24
 python3 -m remote_ikernel manage --add --kernel_cmd="ml purge && ml cuDNN/8.4.1.50-CUDA-11.7.0 && conda activate napari-nemo-jan24 && ipython3 kernel -f {connection_file}" --name="napari-nemo-jan24" --interface=local --workdir="~/"--language=python3
 
