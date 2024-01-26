520e47e241c7d72bd66288fd97c792c2a13aa529
vanessadao31 <vanessadao31@yahoo.co.uk>
2024-01-26 11:30:29 +0000

    run script works
    added conda init bash

--- napari/run_napari.sh
+++ napari/run_napari.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-# load anaconda
+# load anaconda and init
 source ~/.bashrc
 ml Anaconda3
 
 # load other modules
 ml GCC
 ml VirtualGL
+conda init bash
+echo "loaded modules"
 
 # activate environment
+conda deactivate
 echo "activating environment"
 conda activate napari-nemo-jan24
 
