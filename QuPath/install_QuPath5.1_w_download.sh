c8d96507054cf2c31b08d60a9204e12319bf8a33
SaraSalgueiroTorres <salgues@crick.ac.uk>
2025-01-22 15:35:46 +0000

    add install file for 5.1 version

---
+++ QuPath/install_QuPath5.1_w_download.sh
@@ -0,0 +1,32 @@
+\#!/bin/bash
+
+#get file from https://github.com/qupath/qupath/releases/download/v0.5.1/QuPath-v0.5.1-Linux.tar.xz
+
+current_directory=$(pwd)
+QuPath_URL="https://github.com/qupath/qupath/releases/download/v0.5.1/QuPath-v0.5.1-Linux.tar.xz"
+filename=$(basename "$QuPath_URL")
+
+wget -O "$current_directory/$filename" "$QuPath_URL"
+
+#unzip the file
+tar -xf QuPath*
+
+#change directory to the QuPath/bin
+cd $(ls -d QuPath*)
+cd bin
+
+#modify the permissions on QuPath launcher
+chmod u+x QuPath
+
+current_directory=$(pwd)
+rm 
+# Create a file in the home directory to save the current directory
+output_file="$HOME/Run_QuPath.sh"
+
+echo "cd $current_directory/" > "$output_file"
+echo "./QuPath" >> "$output_file"
+
+echo "File created at: $output_file"
+
+cd $HOME
+chmod u+x $output_file
